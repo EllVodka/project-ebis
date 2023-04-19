c23772a681e702dc8a6342331f52535e9d8653b8
Arnaud <arnaudpoivre@gmail.com>
2023-04-19 15:48:53 +0200

    feat(dashboard): change style for the display of the bornes

--- project-ebis/ViewModel/StatViewModel.cs
+++ project-ebis/ViewModel/StatViewModel.cs
@@ -14,22 +14,11 @@ namespace project_ebis.ViewModel
 {
     public partial class StatViewModel : BindableObject
     {
-        public ObservableCollection<string> Resultats { get; set; }
         public ObservableCollection<Borne> Bornes { get; set; } = new();
 
         public StatViewModel()
         {
-            Resultats = new ObservableCollection<string>();
-          
-        }
-
-        public void SelectToDatabase()
-        {
-            var databaseService = new DatabaseService("localhost", "ebis", 3306, "root", "root");
-
-            var conn = databaseService.CreateConnection();
-
-            Resultats = databaseService.ExecuteSelectQuery("SELECT libelle FROM secteur;", conn);
+         
         }
 
         public void GetAllBorne()
